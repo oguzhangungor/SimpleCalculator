13d4bdffbff6a738d0b1c700e1dd4db9313f875b
oguzhangungor <oguzhan.gngr91@gmail.com>
2020-08-12 01:08:31 +0300

    Simple Calculator and use Layout

---
+++ app/src/main/java/com/ogungor/layoutlearning/MainActivity.java
@@ -0,0 +1,83 @@
+package com.ogungor.layoutlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText number1Txt;
+    EditText number2Txt;
+    TextView resultText;
+    int number1;
+    int number2;
+    int result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        number1Txt= findViewById(R.id.number1Text);
+        number2Txt= findViewById(R.id.number2Text);
+        resultText=findViewById(R.id.txtResult);
+
+    }
+
+    public void sum(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(number2Txt.getWindowToken(), 0);
+        if (number1Txt.getText().toString().matches("") || number2Txt.getText().toString().matches("")) {
+            resultText.setText("Enter Number Plase No  Empty Box");
+        }
+        else {
+            number1 = Integer.parseInt(number1Txt.getText().toString());
+            number2 = Integer.parseInt(number2Txt.getText().toString());
+            result = number1 + number2;
+            resultText.setText("Result: " + result);
+        }
+    }
+    public void deduct(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(number2Txt.getWindowToken(), 0);
+        if (number1Txt.getText().toString().matches("") || number2Txt.getText().toString().matches("")) {
+            resultText.setText("Enter Number Plase No  Empty Box");
+        }
+        else {
+            number1 = Integer.parseInt(number1Txt.getText().toString());
+            number2 = Integer.parseInt(number2Txt.getText().toString());
+            result = number1 - number2;
+            resultText.setText("Result: " + result);
+        }
+    }
+    public void multiply(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(number2Txt.getWindowToken(), 0);
+        if (number1Txt.getText().toString().matches("") || number2Txt.getText().toString().matches("")) {
+            resultText.setText("Enter Number Plase No  Empty Box");
+        }
+        else {
+            number1 = Integer.parseInt(number1Txt.getText().toString());
+            number2 = Integer.parseInt(number2Txt.getText().toString());
+            result = number1 * number2;
+            resultText.setText("Result: " + result);
+        }
+    }
+    public void devide(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(number2Txt.getWindowToken(), 0);
+        if (number1Txt.getText().toString().matches("") || number2Txt.getText().toString().matches("")) {
+            resultText.setText("Enter Number Plase No  Empty Box");
+        }
+        else {
+            number1 = Integer.parseInt(number1Txt.getText().toString());
+            number2 = Integer.parseInt(number2Txt.getText().toString());
+            result = number1 / number2;
+            resultText.setText("Result: " + result);
+        }
+    }
+}
