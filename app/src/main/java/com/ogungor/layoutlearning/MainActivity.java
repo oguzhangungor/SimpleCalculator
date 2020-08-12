60ae1ba768903d2ee536b2802fe3b0321bf11919
oguzhangungor <oguzhan.gngr91@gmail.com>
2020-08-12 22:00:09 +0300

    Code Refactor

--- app/src/main/java/com/ogungor/layoutlearning/MainActivity.java
+++ app/src/main/java/com/ogungor/layoutlearning/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,85 +15,65 @@ public class MainActivity extends AppCompatActivity {
     EditText number1Txt;
     EditText number2Txt;
     TextView resultText;
-    int number1;
-    int number2;
-    int result;
+    double number1;
+    double number2;
+    double result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        number1Txt= findViewById(R.id.number1Text);
-        number2Txt= findViewById(R.id.number2Text);
-        resultText=findViewById(R.id.txtResult);
-
+        number1Txt = findViewById(R.id.number1Text);
+        number2Txt = findViewById(R.id.number2Text);
+        resultText = findViewById(R.id.txtResult);
     }
 
-    public boolean control() {
+    public void process(View view) {
+        Button btnProcess = (Button) view;
+        hideKeyboard(btnProcess);
+        calculator(btnProcess.getText().toString());
+    }
 
-        if (number1Txt.getText().toString().matches("") || number2Txt.getText().toString().matches("")) {
-            resultText.setText("Enter Number Please No Empty Box");
-            return true;
+    public boolean isEmpty(EditText editText) {
 
+        return editText.getText().toString().matches("");
+    }
+
+    public void calculator(String processType) {
+
+        if (isEmpty(number1Txt) || isEmpty(number2Txt)) {
+            resultText.setText(getString(R.string.text_not_empty_error));
+        } else {
+
+            number1 = Double.parseDouble(number1Txt.getText().toString());
+            number2 = Double.parseDouble(number2Txt.getText().toString());
+            switch (processType) {
+                case "+":
+
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    if (number2 != 0) {
+                        result = number1 / number2;
+                    } else {
+                        resultText.setText(getString(R.string.text_zero_error));
+                        return;
+                    }
+                    break;
+            }
+            resultText.setText(getString(R.string.text_result, result));
         }
-        return false;
     }
-    public void sum(View view) {
+
+    public void hideKeyboard(View view) {
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(number2Txt.getWindowToken(), 0);
-        if (control()){
-
-        }
-        else {
-            number1 = Integer.parseInt(number1Txt.getText().toString());
-            number2 = Integer.parseInt(number2Txt.getText().toString());
-            result = number1 + number2;
-            resultText.setText("Result: " + result);
-        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
-
-    public void deduct(View view) {
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(number2Txt.getWindowToken(), 0);
-        if (control()){
-
-        }
-        else {
-            number1 = Integer.parseInt(number1Txt.getText().toString());
-            number2 = Integer.parseInt(number2Txt.getText().toString());
-            result = number1 - number2;
-            resultText.setText("Result: " + result);
-        }
-    }
-    public void multiply(View view) {
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(number2Txt.getWindowToken(), 0);
-        if (control()){
-
-        }
-        else {
-            number1 = Integer.parseInt(number1Txt.getText().toString());
-            number2 = Integer.parseInt(number2Txt.getText().toString());
-            result = number1 * number2;
-            resultText.setText("Result: " + result);
-        }
-    }
-    public void devide(View view) {
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(number2Txt.getWindowToken(), 0);
-        if (control()){
-
-        }
-        else if (number2Txt.getText().toString().matches("0"))
-        {
-            resultText.setText("Not zero Number 2");
-        }
-        else {
-            number1 = Integer.parseInt(number1Txt.getText().toString());
-            number2 = Integer.parseInt(number2Txt.getText().toString());
-            result = number1 / number2;
-            resultText.setText("Result: " + result);
-        }
-    }
 }
